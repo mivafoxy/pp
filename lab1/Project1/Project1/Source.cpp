da5c5afb79b1ce82cf3e84ab54e43e1a2b7dfe9e
lainys <is-kirillka@yandex.ru>
2019-03-02 02:07:57 +0300

    all

---
+++ lab1/Project1/Project1/Source.cpp
@@ -0,0 +1,109 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>  
+#include <mpi.h> 
+#include <math.h> 
+
+#define PI 3.141592653589793
+
+double f(double x) {
+	return 1 / (1 + x * x);
+}
+
+// средних прямоугольников
+double f1(int myrank, int nprocs, int n) {
+	double sum = 0, h = 1.0 / n;
+
+	for (int i = myrank + 1; i <= n; i += nprocs) {
+		sum += f(h*(i - 0.5));
+	}
+
+	return 4 * h * sum;
+}
+
+// трапеций
+double f2(int myrank, int nprocs, int n) {
+	double sum = 0, h = 1.0 / n;
+
+	for (int i = myrank + 1; i <= n - 1; i += nprocs) {
+		sum += f(i * h);
+	}
+
+	if (myrank == 0) {
+		sum += (f(0) + f(1)) / 2.0;
+	}
+
+	return 4 * h * sum;
+}
+
+// симпсона
+double f3(int myrank, int nprocs, int n) {
+	double sum = 0, h = 1.0 / n;
+
+	for (int i = myrank + 1; i <= n; i += nprocs) {
+		sum += f((i - 0.5) * h);
+	}
+	sum *= 2;
+
+	for (int i = myrank + 1; i <= n - 1; i += nprocs) {
+		sum += f(i * h);
+	}
+
+	if (myrank == 0) {
+		sum += (f(0) + f(1)) / 2.0;
+	}
+
+	return 4 * h * sum / 3.0;
+}
+
+int main(int argc, char *argv[]) {
+
+	double pi, sum;
+	int myrank, nprocs;
+
+	double t1, t2, allTime = 0;
+
+	int N[] = { 10,50,100,500,1000,5000,10000 };
+
+	MPI_Init(&argc, &argv);
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+	FILE *f = fopen("res.txt", "w");
+
+	for (int fun = 0; fun < 3; fun++) {
+		fprintf(f, "Значение n\t Значение pi\t Время вычисления(в сек.)\t Ошибка вычисления\n");
+
+		for (int i = 0, n = N[i]; i < 7; i++, n = N[i]) {
+			t1 = MPI_Wtime();
+			switch (fun) {
+			case 0:
+				// прямоугольник
+				sum = f1(myrank, nprocs, n);
+				break;
+			case 1:
+				// трапеция
+				sum = f2(myrank, nprocs, n);
+				break;
+			case 2:
+				// симпсон
+				sum = f3(myrank, nprocs, n);
+				break;
+			}
+
+			t2 = MPI_Wtime() - t1;
+
+			MPI_Reduce(&sum, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+			MPI_Reduce(&t2, &allTime, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+			if (myrank == 0) {
+				fprintf(f, "%d\t %.18lg\t %lg\t %lg\n", n, pi, allTime, fabs(pi - PI));
+			}
+		}
+	}
+
+	fclose(f);
+
+	MPI_Finalize();
+	return 0;
+}
---
+++ lab2/lab2/lab2/Source.cpp
@@ -0,0 +1,295 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <mpi.h> 
+#include <stdio.h> 
+
+void show(double** a, int n) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("a[%d][%d] = %lg, ", i, j, a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+double f1(double** a, int* map, int myrank, int nprocs, int n) {
+
+	for (int k = 0; k < n - 1; k++) {
+		if (map[k] == myrank) {
+			for (int i = k + 1; i < n; i++) {
+				a[k][i] /= a[k][k];
+			}
+		}
+
+		MPI_Bcast(&a[k][k + 1], n - k - 1, MPI_DOUBLE, map[k], MPI_COMM_WORLD);
+
+		for (int i = k + 1; i < n; i++) {
+			if (map[i] == myrank) {
+				for (int j = k + 1; j < n; j++) {
+					a[i][j] -= a[i][k] * a[k][j];
+				}
+			}
+		}
+	}
+
+	//if (myrank == 0) {
+	//	show(a, n);
+	//}
+
+	return 0;
+}
+
+// максимальный по столбцу
+double f2(double** a, int* map, int myrank, int nprocs, int n) {
+
+	for (int k = 0; k < n - 1; k++) {
+		int indMax = k;
+		if (map[k] == myrank) {
+
+			double max = a[k][indMax];
+
+			for (int j = k + 1; j < n; j++) {
+				if (map[j] == myrank && a[k][j] > max) {
+					max = a[k][j];
+					indMax = j;
+				}
+			}
+
+			if (k != indMax) {
+				for (int j = 0; j < n; j++) {
+					double temp = a[j][k];
+					a[j][k] = a[j][indMax];
+					a[j][indMax] = temp;
+				}
+			}
+
+			for (int i = k + 1; i < n; i++) {
+				a[k][i] /= a[k][k];
+			}
+		}
+
+		MPI_Status status;
+		for (int j = 0; j < n; j++) {
+			if (map[k] == myrank) {
+				if (map[j] != myrank) {
+					MPI_Send(&a[j][k], 1, MPI_DOUBLE, map[j], 1, MPI_COMM_WORLD);
+					MPI_Send(&indMax, 1, MPI_INT, map[j], 1, MPI_COMM_WORLD);
+					MPI_Send(&a[j][indMax], 1, MPI_DOUBLE, map[j], 1, MPI_COMM_WORLD);
+				}
+
+			}
+			else {
+				if (map[j] == myrank) {
+					int  recvInd;
+					MPI_Recv(&a[j][k], 1, MPI_DOUBLE, map[k], 1, MPI_COMM_WORLD, &status);
+					MPI_Recv(&recvInd, 1, MPI_INT, map[k], 1, MPI_COMM_WORLD, &status);
+					MPI_Recv(&a[j][recvInd], 1, MPI_DOUBLE, map[k], 1, MPI_COMM_WORLD, &status);
+				}
+
+			}
+			//MPI_Bcast(&a[j][k], 1, MPI_DOUBLE, map[k], MPI_COMM_WORLD);
+			//MPI_Bcast(&a[j][indMax], 1, MPI_DOUBLE, map[k], MPI_COMM_WORLD);
+		}
+		
+		MPI_Barrier(MPI_COMM_WORLD);
+
+		for (int i = k + 1; i < n; i++) {
+			if (map[i] == myrank) {
+				for (int j = k + 1; j < n; j++) {
+					a[i][j] -= a[i][k] * a[k][j];
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+// максимальный по столбцу
+double f3(double** a, int* map, int myrank, int nprocs, int n) {
+
+	for (int k = 0; k < n - 1; k++) {
+		int indMax = k;
+		if (map[k] == myrank) {
+
+			double max = a[indMax][k];
+
+			for (int j = k + 1; j < n; j++) {
+				if (map[j] == myrank && a[j][k] > max) {
+					max = a[j][k];
+					indMax = j;
+				}
+			}
+
+			if (k != indMax) {
+				double* temp = a[k];
+
+				a[k] = a[indMax];
+				a[indMax] = temp;
+			}
+
+			for (int i = k + 1; i < n; i++) {
+				a[k][i] /= a[k][k];
+			}
+		}
+
+		MPI_Bcast(&a[indMax][0], n, MPI_DOUBLE, map[k], MPI_COMM_WORLD);
+		MPI_Bcast(&a[k][k + 1], n - k - 1, MPI_DOUBLE, map[k], MPI_COMM_WORLD);
+		MPI_Barrier(MPI_COMM_WORLD);
+
+		for (int i = k + 1; i < n; i++) {
+			if (map[i] == myrank) {
+				for (int j = k + 1; j < n; j++) {
+					a[i][j] -= a[i][k] * a[k][j];
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+
+
+// максимальный по столбцу
+double f4(double** a, int* map, int myrank, int nprocs, int n) {
+
+	for (int k = 0; k < n - 1; k++) {
+		int indMaxR = k, indMaxC = k;
+		if (map[k] == myrank) {
+
+			double max = a[indMaxR][indMaxC];
+
+			for (int i = k; i < n; i++) {
+				for (int j = k; j < n; j++) {
+					if (map[i] == myrank && map[j] == myrank && a[i][j] > max) {
+						max = a[i][j];
+						indMaxR = i;
+						indMaxC = j;
+					}
+				}
+			}
+
+			if (k != indMaxC) {
+				for (int j = 0; j < n; j++) {
+					double temp = a[j][k];
+					a[j][k] = a[j][indMaxC];
+					a[j][indMaxC] = temp;
+				}
+			}
+
+			if (k != indMaxR) {
+				double* temp = a[k];
+
+				a[k] = a[indMaxR];
+				a[indMaxR] = temp;
+			}
+
+			for (int i = k + 1; i < n; i++) {
+				a[k][i] /= a[k][k];
+			}
+		}
+
+		MPI_Status status;
+		for (int j = 0; j < n; j++) {
+			if (map[k] == myrank) {
+				if (map[j] != myrank) {
+					MPI_Send(&a[j][k], 1, MPI_DOUBLE, map[j], 1, MPI_COMM_WORLD);
+					MPI_Send(&indMaxC, 1, MPI_INT, map[j], 1, MPI_COMM_WORLD);
+					MPI_Send(&a[j][indMaxC], 1, MPI_DOUBLE, map[j], 1, MPI_COMM_WORLD);
+				}
+
+			}
+			else {
+				if (map[j] == myrank) {
+					int  recvInd;
+					MPI_Recv(&a[j][k], 1, MPI_DOUBLE, map[k], 1, MPI_COMM_WORLD, &status);
+					MPI_Recv(&recvInd, 1, MPI_INT, map[k], 1, MPI_COMM_WORLD, &status);
+					MPI_Recv(&a[j][recvInd], 1, MPI_DOUBLE, map[k], 1, MPI_COMM_WORLD, &status);
+				}
+
+			}
+			//MPI_Bcast(&a[j][k], 1, MPI_DOUBLE, map[k], MPI_COMM_WORLD);
+			//MPI_Bcast(&a[j][indMax], 1, MPI_DOUBLE, map[k], MPI_COMM_WORLD);
+		}
+
+		MPI_Bcast(&a[indMaxR][0], n, MPI_DOUBLE, map[k], MPI_COMM_WORLD);
+		MPI_Bcast(&a[k][k + 1], n - k - 1, MPI_DOUBLE, map[k], MPI_COMM_WORLD);
+		MPI_Barrier(MPI_COMM_WORLD);
+
+		for (int i = k + 1; i < n; i++) {
+			if (map[i] == myrank) {
+				for (int j = k + 1; j < n; j++) {
+					a[i][j] -= a[i][k] * a[k][j];
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	int myrank, nprocs;
+	int N[] = { 10,50,100,500,1000 };
+	FILE *f = fopen("res.txt", "w");
+
+	MPI_Init(&argc, &argv);
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+	for (int fun = 0; fun < 4; fun++) {
+		fprintf(f, "Значение n\t Время вычислений (в сек.)\t Точность вычислений\n");
+		for (int k = 0, n = N[k]; k < 5; k++, n = N[k]) {
+
+			int *map = new int[n];
+			double **a = new double*[n];
+
+			// инициализация
+			for (int i = 0; i < n; i++) {
+				a[i] = new double[n];
+				for (int j = 0; j < n; j++) {
+					a[i][j] = 1 / (1.0 * (i + j + 1));
+				}
+			}
+
+			for (int i = 0; i < n; i++) {
+				map[i] = i % nprocs;
+			}
+
+			double time = 0, error = 0;
+			switch (fun) {
+			case 0:
+				time = MPI_Wtime();
+				error = f1(a, map, myrank, nprocs, n);
+				time = MPI_Wtime() - time;
+				break;
+			case 1:
+				time = MPI_Wtime();
+				error = f2(a, map, myrank, nprocs, n);
+				time = MPI_Wtime() - time;
+				break;
+			case 2:
+				time = MPI_Wtime();
+				error = f3(a, map, myrank, nprocs, n);
+				time = MPI_Wtime() - time;
+				break;
+			case 3:
+				time = MPI_Wtime();
+				error = f4(a, map, myrank, nprocs, n);
+				time = MPI_Wtime() - time;
+				break;
+			}
+
+			fprintf(f, "%d\t %lg\t %lg\n", n, time, error);
+
+			delete map, a;
+		}
+	}
+
+	MPI_Finalize();
+	fclose(f);
+
+	return 0;
+}
