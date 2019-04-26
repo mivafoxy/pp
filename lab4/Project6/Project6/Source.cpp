130f7969a458b66d4235ef3d2218dfe83a2c2b46
lainys <is-kirillka@yandex.ru>
2019-04-26 22:15:34 +0300

    lab4

---
+++ lab4/Project6/Project6/Source.cpp
@@ -0,0 +1,178 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <CL/cl.h>
+#include <iostream>
+#include <fstream>
+#include <ctime>
+# include <string.h>
+#include <chrono>
+#include "./common/inc/helper_image.h"
+
+unsigned int width = 512, height = 512;
+
+void loadImage(char *file, unsigned char** pixels, unsigned int * width, unsigned int * height)
+{
+	size_t file_length = strlen(file);
+
+	if (!strcmp(&file[file_length - 3], "pgm"))
+	{
+		if (sdkLoadPGM<unsigned char>(file, pixels, width, height) != true)
+		{
+			printf("Failed to load PGM image file: %s\n", file);
+			exit(EXIT_FAILURE);
+		}
+	}
+	return;
+}
+
+void saveImage(char *file, unsigned char* pixels, unsigned int width, unsigned int  height)
+{
+	size_t file_length = strlen(file);
+	if (!strcmp(&file[file_length - 3], "pgm"))
+	{
+		sdkSavePGM(file, pixels, width, height);
+	}
+	return;
+}
+
+const char *g_pcszSource =
+"__kernel void func(__global unsigned char * im_s,__global unsigned char * im_d,unsigned int w,unsigned int h,int radius) \n"
+"{\n"
+"	int i = get_global_id(0);\n"
+"	int tidx = i / w;\n"
+"	int tidy = i % w;\n"
+"	if (tidx < h && tidy < w)\n"
+"	{\n"
+"		unsigned int r = 0;\n"
+"		for (int ir = -radius; ir <= radius; ir++)\n"
+"			for (int ic = -radius; ic <= radius; ic++)\n"
+"			{\n"
+"				int temp_x, temp_y;\n"
+"				if (tidx + ir < 0) {\n"
+"					temp_x = -(tidx + ir);\n"
+"				}\n"
+"				else if (tidx + ir >= h) {\n"
+"					temp_x = (h-1) - ((tidx + ir) - h);\n"
+"				}\n"
+"				else {\n"
+"					temp_x = tidx + ir;\n"
+"				}\n"
+"				if (tidy + ic < 0) {\n"
+"					temp_y = -(tidy + ic);\n"
+"				}\n"
+"				else if (tidy + ic >= w) {\n"
+"					temp_y = (w-1) - ((tidy + ic) - w);\n"
+"				}\n"
+"				else {\n"
+"					temp_y = tidy + ic;\n"
+"				}\n"
+"				r += im_s[temp_x*w + temp_y];\n"
+"			}\n"
+"		r /= ((2 * radius + 1)*(2 * radius + 1));\n"
+"		im_d[tidx*w + tidy] = (unsigned char)r;\n"
+"	}\n"
+"}\n";
+int main(int argc, char ** argv)
+{
+	unsigned char * im_s = NULL;
+	unsigned char * d_pixels = NULL;
+
+	int radius = 5;
+	int N = 10;
+
+	char * src_path = (char*)"lena.pgm";
+	char * d_result_path = (char*)"lena_box_blur.pgm";
+
+	loadImage(src_path, &im_s, &width, &height);
+
+	printf("Image size %dx%d\n", width, height);
+	int image_size = sizeof(unsigned char) * width * height;
+
+	cl_uint uNumPlatforms;
+	clGetPlatformIDs(0, NULL, &uNumPlatforms);
+	cl_platform_id *pPlatforms = new cl_platform_id[uNumPlatforms];
+	clGetPlatformIDs(uNumPlatforms, pPlatforms, &uNumPlatforms);
+
+	cl_device_id deviceID;
+	cl_uint uNumGPU;
+	clGetDeviceIDs(pPlatforms[0], CL_DEVICE_TYPE_DEFAULT, 1, &deviceID, &uNumGPU);
+
+	cl_int errcode_ret;
+	cl_context context = clCreateContext(NULL, 1, &deviceID, NULL, NULL, &errcode_ret);
+
+	errcode_ret = 0;
+	cl_command_queue queue = clCreateCommandQueue(context, deviceID, CL_QUEUE_PROFILING_ENABLE, &errcode_ret);
+
+	errcode_ret = CL_SUCCESS;
+	size_t source_size = strlen(g_pcszSource);
+	cl_program program = clCreateProgramWithSource(context, 1, &g_pcszSource, (const size_t *)&source_size, &errcode_ret);
+
+	cl_int errcode = clBuildProgram(program, 1, &deviceID, NULL, NULL, NULL);
+
+	cl_kernel kernel = clCreateKernel(program, "func", NULL);
+
+	std::ofstream res("text.txt");
+
+
+	
+	for (int i = radius; i < radius + N; i++) {
+
+		// инициализация
+		cl_mem buffer_im_s = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, image_size, im_s, NULL);
+		cl_mem buffer_im_d = clCreateBuffer(context, CL_MEM_WRITE_ONLY, image_size, im_s, NULL);
+
+		// установка аргументов
+		clSetKernelArg(kernel, 0, sizeof(buffer_im_s), (void *)&buffer_im_s);
+		clSetKernelArg(kernel, 1, sizeof(buffer_im_d), (void *)&buffer_im_d);
+		clSetKernelArg(kernel, 2, sizeof(unsigned int), (void *)&width);
+		clSetKernelArg(kernel, 3, sizeof(unsigned int), (void *)&height);
+		clSetKernelArg(kernel, 4, sizeof(int), (void *)&i);
+
+		// запуск ядра
+		cl_event event;
+		size_t uGlobalWorkSize = width * height;
+
+		clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &uGlobalWorkSize, NULL, 0, NULL, &event);
+
+		clWaitForEvents(1, &event);
+		clFinish(queue);
+
+		cl_ulong time_start;
+		cl_ulong time_end;
+
+		clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_START, sizeof(time_start), &time_start, NULL);
+		clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_END, sizeof(time_end), &time_end, NULL);
+
+		// отображение результата
+		d_pixels = new unsigned char[width*height];
+		cl_uchar *puData = (cl_uchar *)clEnqueueMapBuffer(queue, buffer_im_d, CL_TRUE, CL_MAP_READ, 0, image_size, 0, NULL, NULL, NULL);
+		for (int i = 0; i < width*height; ++i) {
+			//std::cout << i << " = " << puData[i] << "; ";
+			d_pixels[i] = puData[i];
+		}
+		char* fileName = new char[17];
+		sprintf(fileName, "result_bb_%02d.pgm", i);
+		saveImage(fileName, d_pixels, width, height);
+		//std::cout << std::endl;
+		clEnqueueUnmapMemObject(queue, buffer_im_d, puData, 0, NULL, NULL);
+		//
+
+		clReleaseMemObject(buffer_im_s);
+		clReleaseMemObject(buffer_im_d);
+		double t1 = (time_end - time_start) / 1000000000.0;
+
+
+		res << i << "\t"<< t1 << std::endl;
+	}
+
+
+	res.close();
+
+	clReleaseKernel(kernel);
+	clReleaseProgram(program);
+	clReleaseCommandQueue(queue);
+	clReleaseContext(context);
+	delete[] pPlatforms;
+	delete d_pixels;
+
+	system("pause");
+}
---
+++ lab4/dfgfd/dfgfd/kernel.cu
@@ -0,0 +1,171 @@
+﻿# include <time.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <cuda.h>
+# include <ctime>
+#include <cuda_runtime.h>
+#include "./common/inc/helper_image.h"
+using namespace std;
+
+float checkGPU(unsigned char * d_result_pixels, int radius, int k);
+float cudaPallel(unsigned char * d_result_pixels, int radius);
+
+texture<unsigned char, 2, cudaReadModeElementType> g_Texture;
+unsigned int width = 512, height = 512;
+
+
+__global__  void BoxBlur_kernel(unsigned char * pDst, int radius, int w, int h)
+{
+	int tidx = threadIdx.x + blockIdx.x * blockDim.x;
+	int tidy = threadIdx.y + blockIdx.y * blockDim.y;
+	if (tidx < w && tidy < h)
+	{
+		unsigned int r = 0;
+		for (int ir = -radius; ir <= radius; ir++)
+			for (int ic = -radius; ic <= radius; ic++)
+			{
+				r += tex2D(g_Texture, tidx + 0.5f + ic, tidy + 0.5f + ir);
+			}
+		r /= ((2 * radius + 1)*(2 * radius + 1));
+		pDst[tidx + tidy * w] = (unsigned char)r;
+	}
+}
+
+void loadImage(char *file, unsigned char** pixels, unsigned int * width, unsigned int * height)
+{
+	size_t file_length = strlen(file);
+
+	if (!strcmp(&file[file_length - 3], "pgm"))
+	{
+		if (sdkLoadPGM<unsigned char>(file, pixels, width, height) != true)
+		{
+			printf("Failed to load PGM image file: %s\n", file);
+			exit(EXIT_FAILURE);
+		}
+	}
+	return;
+}
+
+void saveImage(char *file, unsigned char* pixels, unsigned int width, unsigned int  height)
+{
+	size_t file_length = strlen(file);
+	if (!strcmp(&file[file_length - 3], "pgm"))
+	{
+		sdkSavePGM(file, pixels, width, height);
+	}
+	return;
+}
+
+int main(int argc, char ** argv)
+{
+	unsigned char * d_result_pixels;
+	unsigned char * h_result_pixels;
+	unsigned char * h_pixels = NULL;
+	unsigned char * d_pixels = NULL;
+
+	int radius = 5;
+
+	char * src_path = "lena.pgm";
+	char * d_result_path = "lena_box_blur.pgm";
+
+	loadImage(src_path, &h_pixels, &width, &height);
+
+	printf("Image size %dx%d\n", width, height);
+
+	int image_size = sizeof(unsigned char) * width * height;
+
+	h_result_pixels = (unsigned char *)malloc(image_size);
+	cudaMalloc((void **)& d_pixels, image_size);
+	cudaMalloc((void **)& d_result_pixels, image_size);
+	cudaMemcpy(d_pixels, h_pixels, image_size, cudaMemcpyHostToDevice);
+
+
+	cudaChannelFormatDesc desc = cudaCreateChannelDesc<uchar1>();
+	cudaError_t error = cudaBindTexture2D(0, &g_Texture, d_pixels, &desc, width, height, width * sizeof(unsigned char));
+
+	if (cudaSuccess != error) {
+		printf("ERROR: Failed to bind texture.\n");
+		exit(-1);
+	}
+	else {
+		printf("Texture was successfully binded\n");
+	}
+
+	int N = 10;
+
+	int* rs = new int[N];
+	float* ans1 = new float[N];
+
+	for (int i = radius; i < radius + N; i++) {
+		rs[i - radius] = i;
+		ans1[i - radius] = checkGPU(d_result_pixels, i, 1);
+		std::cout << i << std::endl;
+		char* fileName = new char[17];
+		sprintf(fileName, "result_bb_%02d.pgm", i);
+		cudaMemcpy(h_result_pixels, d_result_pixels, image_size, cudaMemcpyDeviceToHost);
+		saveImage(fileName, h_result_pixels, width, height);
+	}
+
+	std::ofstream out("text1.txt", 'w');
+
+	for (int i = 0; i < N; i++) {
+		out << rs[i] << "\t" << ans1[i] << std::endl;
+	}
+
+	out.close();
+
+
+	cudaUnbindTexture(&g_Texture);
+
+	cudaFree(d_pixels);
+	cudaFree(d_result_pixels);
+
+	delete rs, ans1;
+
+	return 0;
+}
+
+float checkGPU(unsigned char * d_result_pixels, int radius, int k) {
+	float time = 0;
+
+	for (int i = 0; i < k; i++) {
+		time += cudaPallel(d_result_pixels, radius);
+	}
+
+	return time / (1000.0f * k);
+}
+
+float cudaPallel(unsigned char * d_result_pixels, int radius) {
+
+	int n = 16;
+	dim3 block(n, n);
+	dim3 grid(width / n, height / n);
+
+	//----
+	cudaEvent_t start, stop;
+	float gpuTime = 0.0f;
+
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
+	cudaEventRecord(start, 0);
+	//----
+	BoxBlur_kernel << < grid, block >> > (d_result_pixels, radius, width, height);
+	//negative_kernel << < grid, block >> >(d_result_pixels, width, height);
+
+	//----
+	cudaEventRecord(stop, 0);
+
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&gpuTime, start, stop);
+
+	//printf("N, time spent executing by the GPU: %.5f ms\n", gpuTime);
+
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
+	//----
+	/* CUDA method */
+
+	return gpuTime;
+}
